2b7a283513322d0140c56aa998ad75c094b22c75
usmansajid00 <usmansajid2517@gmail.com>
2024-07-28 23:23:22 +0500

    creating HomePage.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import HeaderImg from "../utils/Images/Header.png";
+
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+`;
+const Section = styled.div``;
+const Img = styled.img``;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section style={{ alignItems: "center" }}>
+        <Img src={HeaderImg} />
+      </Section>
+    </Container>
+  );
 };
 
 export default Home;
