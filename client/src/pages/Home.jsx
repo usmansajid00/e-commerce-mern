0a562e10d0686f59e099e647afe322c628f0c938
usmansajid00 <usmansajid2517@gmail.com>
2024-07-30 23:29:34 +0500

    creating Home page sections.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,15 +7,43 @@ const Container = styled.div`
   height: 100%;
   overflow-y: scroll;
   display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  @media (max-width: 768) {
+    padding: 20px 12px;
+  }
+  background: ${({ theme }) => theme.bg};
 `;
-const Section = styled.div``;
-const Img = styled.img``;
+const Section = styled.div`
+  max-width: 1400px;
+  padding: 32px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+`;
+const Img = styled.img`
+  width: 100%;
+  height: 700px;
+  object-fit: cover;
+  max-width: 1200px;
+`;
+const Title = styled.div`
+  font-size: 28px;
+  font-weight: 500;
+  display: flex;
+  justify-content: ${({ center }) => (center ? "center" : "space-between")};
+  align-items: center;
+`;
 
 const Home = () => {
   return (
     <Container>
       <Section style={{ alignItems: "center" }}>
         <Img src={HeaderImg} />
+      </Section>{" "}
+      <Section style={{ alignItems: "center" }}>
+        <Title>Shop by Categories</Title>
       </Section>
     </Container>
   );
